21ea7b1c75ce252f074a0fd79a2a30b1b706f747
Kate Christensen <katec125@gmail.com>
2020-07-19 19:38:10 -0500

    finished code for blog post

--- blog_post_7_19.py
+++ blog_post_7_19.py
@@ -22,6 +22,29 @@ for head in elim_chart.findAll('th'):
     if head.text.startswith('Elim') is False:
         header_list.append(head.text.rstrip())
 
+# obtain meanings of the different colors
+meaning_list = soup.find('b', text=re.compile('Colour key:')).find_all_next('dd', limit = 6)
+
+color_list = []
+meaning_text_list = []
+for item in meaning_list:
+    color_list.append(re.split('(?:background-color:)(\w*)',item.span.get('style'))[1])
+    color_list = [color.lower() for color in color_list]
+    meaning_text_list.append(re.split('(– )',item.text)[-1])
+
+full_text_meaning_dict = dict(zip(color_list, meaning_text_list))
+
+shortened_text_list = ['next_round',
+'eliminated',
+'least_fav',
+'fav',
+'runner_up',
+'winner',]
+
+shortened_meaning_dict = dict(zip(color_list, shortened_text_list))
+shortened_meaning_dict['silver'] = 'not_in_comp'
+
+
 # get list of dicts, 
 # each dict contains column name and entry for that row
 row_list = []
@@ -33,32 +56,15 @@ for name in elim_chart.findAll('td', align = 'left'):
         col = 1
         if item.has_attr('colspan'):
             col = int(item.get('colspan'))
-        row.extend([x for x in 
+        row.extend([shortened_meaning_dict[x.lower()] for x in 
         re.split("(?:background:\s?)(\w*)(?:;)”?", color) 
         if x] * col)
     row_list.append(dict(zip(header_list,row)))
 
 df = pd.DataFrame.from_dict(row_list)
+df.set_index('Baker', inplace=True)
+t_df = df.transpose()
 
-
-meaning_list = soup.find('b', text=re.compile('Colour key:')).find_all_next('dd', limit = 6)
-
-color_list = []
-meaning_text_list = []
-for item in meaning_list:
-    color_list.append(re.split('(?:background-color:)(\w*)',item.span.get('style'))[1])
-    meaning_text_list.append(re.split('(– )',item.text)[-1])
-meaning_text_list
-
-full_text_meaning_dict = dict(zip(color_list, meaning_text_list))
-full_text_meaning_dict['LightBlue'] = 'next_round'
-full_text_meaning_dict['OrangeRed'] = 'eliminated'
-full_text_meaning_dict['Plum'] = 'least_fav'
-full_text_meaning_dict['CornflowerBlue'] = 'fav'
-full_text_meaning_dict['LimeGreen'] = 'runner_up'
-full_text_meaning_dict['Yellow'] = 'winner'
-full_text_meaning_dict['silver'] = 'not_in_comp'
-
-
-
+pd.get_dummies(df)
+pd.get_dummies(t_df)
 
